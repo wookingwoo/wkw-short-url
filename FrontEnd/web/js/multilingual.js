2df23da20f335c8a2142d2420f34bcfb30bdbb93
JeongHeon Woo <contact@wookingwoo.com>
2022-01-04 03:34:17 +0900

    Add guide descriptions

--- FrontEnd/web/js/multilingual.js
+++ FrontEnd/web/js/multilingual.js
@@ -12,6 +12,14 @@ const lang = {
     animal_face: "animal face",
     wookingwoo_maptool: "maptool",
     wookingwoo_msg: "web SMS",
+
+    description1: "wkw.one is a free service that shortens long URLs.",
+    description2: "You can use simple URLs for link sharing or text transmission.",
+    description3: "If you use this for malicious purposes, it can be deleted.",
+    description4: "It can be restricted if daily allowed traffic is exceeded. If you have a lot of usage, please contact us by email.",
+    description5: "If you need a custom domain(designated URL), please contact us by email.",
+    email_inquiry: "Email inquiry: ",
+
   },
   ko: {
     title: "wkw.one - 우킹우 단축 URL",
@@ -26,6 +34,14 @@ const lang = {
     animal_face: "동물상 테스트",
     wookingwoo_maptool: "지도",
     wookingwoo_msg: "인터넷 문자",
+
+    description1: "wkw.one은 긴 URL 주소를 짧게 줄여주는 무료 서비스입니다.",
+    description2: "링크공유, 문자전송등에 간편한 URL을 사용할 수 있습니다.",
+    description3: "악의적 목적으로 사용될 경우 삭제조치 될 수 있습니다.",
+    description4: "일일 허용 트래픽 초과시 주소 변환이 제한될 수 있습니다. (일일 사용량이 많을 경우 이메일 문의 바랍니다.)",
+    description5: "커스텀 도메인, 지정 URL이 필요한 경우 이메일 문의 바랍니다.",
+    email_inquiry: "이메일 문의: ",
+
   },
 };
 
